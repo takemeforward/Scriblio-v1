54f30e950032010d59e0f026e8ffcc843a281603
Vishal Jha <mailkaro0121@gmail.com>
2023-06-22 01:42:01 +0530

    like, comment, image upload all work good

--- app.js
+++ app.js
@@ -339,6 +339,6 @@ app.get("/userinfo", (req, res)=>{
     res.json(userLoginInfo);
     }
 });
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
